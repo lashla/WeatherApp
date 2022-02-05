574bb44f16673edd356279ed01eb0f4291ff22f9
lashla <lash.la412@gmail.com>
2022-02-05 16:21:03 +0300

    nichego ne mogunaiti

--- app/src/main/java/com/example/weatherapp/api/RetrfitInstance.kt
+++ app/src/main/java/com/example/weatherapp/api/RetrfitInstance.kt
@@ -1,14 +1,31 @@
 package com.example.weatherapp.api
 
+import com.example.weatherapp.model.Request
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 
 object RetrfitInstance {
-    private val api: WeatherApi by lazy{
-        Retrofit.Builder()
+    fun getData() {
+        val api: WeatherApi = Retrofit.Builder()
             .baseUrl("http://api.weatherstack.com")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(WeatherApi::class.java)
+
+        val retrofitData = api.getPost("2486c00d678c12f26979dcefa4344b2f")
+        retrofitData.enqueue(object: Callback<Request>{
+            override fun onResponse(call: Call<Request>, response: Response<Request>) {
+                val responseBody = response.body()
+
+            }
+
+            override fun onFailure(call: Call<Request>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
--- app/src/main/java/com/example/weatherapp/api/WeatherApi.kt
+++ app/src/main/java/com/example/weatherapp/api/WeatherApi.kt
@@ -2,13 +2,17 @@ package com.example.weatherapp.api
 
 
 import com.example.weatherapp.model.Current
+import com.example.weatherapp.model.Location
 import com.example.weatherapp.model.Post
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Request
 import okhttp3.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApi {
     @GET("/current")
-    fun getPost(@Query("2486c00d678c12f26979dcefa4344b2f") key: String): Response<Current>
+    fun getPost(@Query("2486c00d678c12f26979dcefa4344b2f") key: String): Callback<com.example.weatherapp.model.Request>
 
 }
