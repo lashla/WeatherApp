5835d280affdfd2450475fec3b2e1ac59168ad30
lashla <lash.la412@gmail.com>
2022-02-06 14:33:16 +0300

    Made some backEnd

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,32 +1,39 @@
 package com.example.weatherapp
 
+import android.app.DownloadManager
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
+import android.util.Log
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import com.example.weatherapp.api.WeatherApi
 import com.example.weatherapp.api.WeatherResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.ArrayList
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
-    var inputCityName: String = ""
+    private val cityArrayList = ArrayList<String>()
     private lateinit var textInput: EditText
     private lateinit var enteredCityName: TextView
     private lateinit var imageView: ImageView
     private lateinit var tvTemperature: TextView
-
+    private lateinit var query: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val enteredCityName: TextView = findViewById(R.id.enteredCityName)
         findViewById<EditText?>(R.id.textInput).setOnEditorActionListener { v, actionId, event ->
             return@setOnEditorActionListener when (actionId) {
                 EditorInfo.IME_ACTION_SEND -> {
-
+                    query = v.toString()
                     true
                 }
                 else -> false
@@ -41,7 +48,19 @@ class MainActivity : AppCompatActivity() {
         val service = retrofit.create(WeatherApi::class.java)
 
         val call = service.getCityTemp(query, "2486c00d678c12f26979dcefa4344b2f")
+            call.enqueue(object : Callback<WeatherResponse>{
+                override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>){
+                    if (response.code() == 200) {
+                        cityArrayList.add(response.body()!!.name!!)
+                    }
+            }
 
-    }
+                override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
+                    Log.e(TAG, "onFailure: enqueing data", )
+                }
+            })
 }
+}
+
+
 
--- app/src/main/java/com/example/weatherapp/api/WeatherResponse.kt
+++ app/src/main/java/com/example/weatherapp/api/WeatherResponse.kt
@@ -3,8 +3,8 @@ package com.example.weatherapp.api
 import com.google.gson.annotations.SerializedName
 
 class WeatherResponse {
-    @SerializedName("temp")
-    var temp: Int = 0
+    @SerializedName("temperature")
+    var temperature: Int = 0
     @SerializedName("name")
     var name: String? = null
 }
