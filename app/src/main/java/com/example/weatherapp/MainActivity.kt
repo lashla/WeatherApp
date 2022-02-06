9e126bb10aacd29b9dd2e89a43aadac44aa3c5fc
lashla <lash.la412@gmail.com>
2022-02-06 16:29:02 +0300

    editText works

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,56 +1,27 @@
 package com.example.weatherapp
 
-import android.app.DownloadManager
-import android.content.ContentValues.TAG
+import kotlinx.android.synthetic.main.activity_main.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Html
-import android.util.Log
 import android.view.KeyEvent
-import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
-import com.example.weatherapp.api.WeatherApi
-import com.example.weatherapp.api.WeatherResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.ArrayList
-import kotlin.properties.Delegates
+
 
 class MainActivity : AppCompatActivity() {
-    private val cityArrayList = ArrayList<String>()
-    private var currentWeather: Int = 0
-    private lateinit var textInput: EditText
-    private lateinit var enteredCityName: TextView
-    private lateinit var imageView: ImageView
-    private lateinit var tvTemperature: TextView
-    private lateinit var query: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textInput = findViewById(R.id.textInput)
-        textInput.setOnKeyListener(object : View.OnKeyListener{
-            override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
-                if (event != null) {
-                    if (event.action == KeyEvent.ACTION_DOWN &&
-                        keyCode == KeyEvent.KEYCODE_ENTER) {
-                        Toast.makeText(applicationContext, "pressed some key$textInput", Toast.LENGTH_LONG).show()
+        textInput.setOnEditorActionListener { _, keyCode, event ->
+            if (((event?.action ?: -1) == KeyEvent.ACTION_DOWN)
+                || keyCode == EditorInfo.IME_ACTION_DONE) {
 
-                        textInput.clearFocus()
-                        textInput.isCursorVisible = false
+                Toast.makeText(applicationContext,"you've entered ${textInput.text}", Toast.LENGTH_LONG).show()
 
-                        return true
-                    }
-                }
-                return false
+                return@setOnEditorActionListener true
             }
-        })
+            return@setOnEditorActionListener false
+        }
 
 
 
