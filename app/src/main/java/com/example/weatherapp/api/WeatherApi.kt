22bef84921ba4932cc9e739944f68936fd9f2a2e
LasHLA <45652713+lashla@users.noreply.github.com>
2022-03-15 18:10:39 +0300

    Update WeatherApi.kt

--- app/src/main/java/com/example/weatherapp/api/WeatherApi.kt
+++ app/src/main/java/com/example/weatherapp/api/WeatherApi.kt
@@ -6,8 +6,6 @@ import retrofit2.http.Query
 
 interface WeatherApi {
 
-    //У тебя тут были неправильные названия get параметров
-
     @GET("/current")
     fun getCityTemp(
         @Query("query") name: String,
